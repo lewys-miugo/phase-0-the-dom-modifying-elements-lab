86d27ba567f00af150ef426780029a9b8c645553
lewys-miugo <lewiswambugu01@gmail.com>
2025-06-29 16:41:49 +0300

    writing js  to dom manipulate

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Step 1: Remove the <main id="main">
+const main = document.getElementById("main");
+main.remove();
+
+// Step 2: Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Step 3: Assign it an id of 'victory'
+newHeader.id = "victory";
+
+// Step 4: Set the innerHTML to "[Your Name] is the champion"
+newHeader.innerHTML = "Lewis Wambugu is the champion";
+
+// Step 5: Append the new header to the document body
+document.body.appendChild(newHeader);
